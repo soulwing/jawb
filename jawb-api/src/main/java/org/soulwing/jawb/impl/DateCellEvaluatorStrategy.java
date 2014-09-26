3014bdab44e6cf093acc27d39400624bce915c27
Carl Harris <ceharris@vt.edu>
2014-09-26 09:20:34 -0400

    replaced copyright

--- jawb-api/src/main/java/org/soulwing/jawb/impl/DateCellEvaluatorStrategy.java
+++ jawb-api/src/main/java/org/soulwing/jawb/impl/DateCellEvaluatorStrategy.java
@@ -1,7 +1,7 @@
 /*
  * File created on Dec 17, 2013 
  *
- * Copyright (c) 2013 Virginia Polytechnic Institute and State University
+ * Copyright (c) 2013 Carl Harris, Jr.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
